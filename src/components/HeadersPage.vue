b6d2e75cde1e332aee004b263a7f4e03df3d0c20
Alifor <116239087+alinfor@users.noreply.github.com>
2023-09-05 08:59:46 +0200

    First commit

---
+++ src/components/HeadersPage.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header className="bg-[#003141] py-4">
+    <div className="container mx-auto flex justify-between items-center">
+      <div>
+        <img src="../assets/logo.jpg" class="h-13 w-20">
+
+      </div>
+      <nav>
+        <!-- <ul className="flex space-x-4">
+          <li><a href="#" className="text-white hover:text-gray-400">Accueil</a></li>
+          <li><a href="#" className="text-white hover:text-gray-400">À propos</a></li>
+          <li><a href="#" className="text-white hover:text-gray-400">Services</a></li>
+          <li><a href="#" className="text-white hover:text-gray-400">Contact</a></li>
+        </ul> -->
+      </nav>
+    </div>
+  </header>
+</template>
+
